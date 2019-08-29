b5ffe1e524e22ace791f53faa10245e4d62d32fd
rahul2106 <rahul.kmr2106@gmail.com>
2019-08-29 15:52:51 +0530

    Solution

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -4,10 +4,23 @@ using System.Linq;
 namespace CaesarCipher
 {
     public static class RotationalCipher
-    {
+    {private static char Cipher(char ch, int key)
+{
+	if (!char.IsLetter(ch))
+		return ch;
+
+	char offset = char.IsUpper(ch) ? 'A' : 'a';
+	return (char)((((ch + key) - offset) % 26) + offset);
+}
+        
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+          string output = string.Empty;
+
+	foreach (char ch in text)
+		output += Cipher(ch, shiftKey);
+
+	return output;
         }
         public static void Main(string[] args){
             
